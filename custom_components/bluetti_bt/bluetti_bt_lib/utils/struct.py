037468cee4049050f9629fea4693ebf49d35323c
Adalberto Garcia Garces <atiestorage@gmail.com>
2026-01-16 01:15:39 -0300

    Fix DecimalArrayField type hint: Decimal -> List[Decimal]

--- custom_components/bluetti_bt/bluetti_bt_lib/utils/struct.py
+++ custom_components/bluetti_bt/bluetti_bt_lib/utils/struct.py
@@ -107,7 +107,7 @@ class DecimalArrayField(DeviceField):
         self.scale = scale
         super().__init__(name, address, size)
 
-    def parse(self, data: bytes) -> Decimal:
+    def parse(self, data: bytes) -> List[Decimal]:
         values = list(struct.unpack(f"!{self.size}H", data))
         return [Decimal(v) / 10 ** self.scale for v in values]
 
